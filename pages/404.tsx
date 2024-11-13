a0015a18f931f677658fe3e2f4f5d3479ec70bc1
Chaitanya <chaitu.vsh@gmail.com>
2024-11-12 22:59:19 -0800

    Adding a custom layout component, specific to certain pages

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,4 +4,19 @@ const _404 = () => {
   )
 }
 
+/**
+ * Handler to return custom layout for this component.
+ * @param componentContent, component that needs to be wrapped with this custom layout
+ * @returns this component with updated layout.
+ */
+_404.getLayout = (componentContent) => {
+  return (
+    <>
+      <p>Unfortunately you landed on non existing page. <b>Served using custom component layouts.</b></p>
+      <br></br>
+      {componentContent}
+    </>
+  )
+};
+
 export default _404;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const getLayout = Component.getLayout ?? ((component) => component)
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </Layout>
   );
 }
