32cbfbe6549061d787129522373921d02cd7c5c0
Chaitanya <chaitu.vsh@gmail.com>
2024-11-13 23:20:02 -0800

    Dynamic links with slugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,43 @@
 import Link from "next/link";
 
 const index = () => {
+  const users = [
+    {
+      id: 1,
+      age: 10,
+      name: "test"
+    },
+    {
+      id: 2,
+      age: 30,
+      name: "test2"
+    },
+  ];
+
   return (
     <div>
       <h3>This is a home page</h3>
-      <p><Link href={'/blog/2'}>We are linking to blog 2 page</Link></p>
+      <p>Below is list of dynamic users list links</p>
+      <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          <Link href={{
+            pathname: `/user/[id]/[age]`,
+            query: {id: user.id, age: user.age}
+          }}>
+            {user.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
     </div>
   )
 }
+
+/**
+ * pathname: `/user/[id]/[age]`,
+            query: {id: user.id, age: user.age}
+  takes us to /user/1/10 for given sample data.
+ */
 
 export default index;
