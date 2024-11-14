f107b3c1cd1e446fbe2db10254cf847f9cfdb92b
Chaitanya <chaitu.vsh@gmail.com>
2024-11-13 22:48:13 -0800

    Links and Redirecting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 const index = () => {
   return (
     <div>
       <h3>This is a home page</h3>
+      <p><Link href={'/blog/2'}>We are linking to blog 2 page</Link></p>
     </div>
   )
 }
--- pages/user/[slug].tsx
+++ pages/user/[slug].tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router'
 
 const Blog = () => {
     const router = useRouter();
-    return <p>Blog number: {router.query.slug}</p>
+    return <p>User details: {router.query.slug}</p>
 }
 
 export default Blog;
